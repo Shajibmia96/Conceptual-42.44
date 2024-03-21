a43b4e36265a0681b0250794edb7512828e005d4
Shajib96 <shajibahamedornob@gmail.com>
2024-03-21 09:57:38 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='bg-fuchsia-200'>Hello world!</div>,
+  },
+
+  {
+    path : "/product",
+    element: <div>This is product components</div>
+  }
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
